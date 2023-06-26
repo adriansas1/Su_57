6b77ea1495e69202217022e3d2231c16bfc2dc1f
Nguyen Phuc Huy <adrian@Nguyens-MacBook-Air.local>
2023-06-27 06:02:42 +1000

    push js file

---
+++ script.js
@@ -0,0 +1,16 @@
+let valueDisplays = document.querySelectorAll(".stat-counter");
+let interval = 4000;
+
+//** start value -> end value to parse -> duration of time*/ 
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-decimals"));
+    let duration =  Math.floor(interval/endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration)
+});
